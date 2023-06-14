a28698ef65c5728536d97be486990818e2c3314d
Leonellazcano <leonellazcano.r@gmail.com>
2023-06-14 12:09:36 -0300

    Primer commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+const Error = ({children}) => {
+  return ( 
+  <div className=" bg-red-800 text-white text-center p-3 mb-3 uppercase font-bold m-3 rounded-md ">
+       {children}
+  </div>)
+  
+}
+
+export default Error
